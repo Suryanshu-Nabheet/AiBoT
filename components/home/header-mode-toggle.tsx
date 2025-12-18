bf6354cc4a86026e9be7f048afbeb22174f440a6
Suryanshu Nabheet <suryanshunab@gmail.com>
2025-12-18 07:32:28 +0530

    Code

--- components/home/header-mode-toggle.tsx
+++ components/home/header-mode-toggle.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { ModeToggle } from "@/components/home/mode-toggle";
 import { useViewMode } from "@/contexts/view-mode-context";
 
 export function HeaderModeToggle() {
+  const pathname = usePathname();
   const { viewMode, setViewMode } = useViewMode();
+
+  if (pathname !== "/") return null;
+
   return <ModeToggle mode={viewMode} onChange={setViewMode} className="ml-2" />;
 }
